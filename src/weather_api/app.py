a2fae52b39e37154b5ca1b02104c06dc3ffe1ae5
0x6f736f646f <blackd0t@protonmail.com>
2020-03-03 12:40:10 +0300

    Adding app file

---
+++ src/weather_api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+from schemas.schema import our_schema
+from models.model import app
+import time, atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from Utility.job import cron
+
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=our_schema, graphiql=True))
+
+@app.route('/')
+def index():
+    return "GraphQL server is listening on /graphql"
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=cron, trigger="interval", minutes=5)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+if __name__ == "__main__":
+    app.run()
+
